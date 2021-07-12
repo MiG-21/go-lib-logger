349dc3e7185a4dd8ef5e8e811bcfde1ee62d8be8
Gaspar K <gaspar.karakulyan@pluto.tv>
2021-07-12 20:22:20 +0300

    actions workflow has been improved

--- .github/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Run the go-vet tool.
-go vet ./..           || exit 1
-# Run the test-cases, with race-detection.
-go test -race ./...   || exit 1
-# Everything passed, exit cleanly.
-exit 0
